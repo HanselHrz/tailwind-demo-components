df15d0ba082d35f3e9b56352171e1b19435bb884
HanselHrz <hansel.hernandezc@gmail.com>
2024-08-08 01:57:43 -0600

    Inicializacion componentes

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Input } from "./components/ui/Input"
+import { Title } from "./components/ui/Title"
+
+
+function App() {
+
+  return (
+    <>
+    <Title>Prueba de componentes</Title>
+    <Input placeholder="Prueba" type="text"/>
+    </>
+  )
+}
+
+export default App
